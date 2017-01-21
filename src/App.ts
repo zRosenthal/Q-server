a4c7b6fa16716c9d322106403524758875df41c1
Ryan Cornillie <cornil15@msu.edu>
2017-01-21 08:13:53 -0500

    working office delete

--- src/App.ts
+++ src/App.ts
@@ -43,6 +43,7 @@ class App {
     private middleware(): void {
         this.express.use(function (req, res, next) {
             res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Methods', 'DELETE');
             res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
             next();
         });
