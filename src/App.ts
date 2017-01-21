88d563b58df50ae3d8d8c7210c8f2d7bbae88866
Zachary Rosenthal <rosent76@msu.edu>
2017-01-20 19:05:57 -0500

    fixed test route

--- src/App.ts
+++ src/App.ts
@@ -54,9 +54,10 @@ class App {
         /* This is just to get up and running, and to make sure what we've got is
          * working so far. This function will change when we start to add more
          * API endpoints */
-        let router = express.Router();
         // placeholder route handler
-        router.get('/', (req, res, next) => {
+        this.express.get('/', (req, res, next) => {
+
+            console.log('this route');
             res.json({
                 message: 'Hello World!'
             });
